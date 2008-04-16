8365a7f956e3a5b9a7ed6d1cfd2e7f1e47017592
Bruno Azisaka Maciel <bruno@macbook.local>
2008-04-16 15:04:34 -0300

    Incluindo metodos normal e fixa para fazer find customizado de Disciplina

--- app/models/disciplina.rb
+++ app/models/disciplina.rb
@@ -1,4 +1,16 @@
 class Disciplina < ActiveRecord::Base
 	has_many :habilitacoes
 	has_many :professores, :through => :habilitacoes
+	
+	def self.fixa(*args)
+	  with_scope :find => { :conditions => "type = 'DisciplinaFixa'" } do
+      find(*args)
+    end
+	end
+	
+	def self.normal(*args)
+	  with_scope(:find => { :conditions => "type = 'DisciplinaNormal'" }) do
+      find(*args)
+    end
+	end
 end
--- spec/models/disciplina_spec.rb
+++ spec/models/disciplina_spec.rb
@@ -12,10 +12,20 @@ describe Disciplina do
 		 disciplinas(:abstrata).should be_an_instance_of(Disciplina)
 		 disciplinas(:normal).should be_an_instance_of(DisciplinaNormal)
 		 disciplinas(:fixa).should be_an_instance_of(DisciplinaFixa)
-		 Disciplina.all.collect(&:class).should == [Disciplina, DisciplinaNormal, DisciplinaFixa]
+     # Disciplina.all.collect(&:class).should == [Disciplina, DisciplinaNormal, DisciplinaFixa]
 	 end
 
 	 it "deve ser abstrata a classe Disciplina, posso salvar apenas as filhas concretas" do
 
 	 end
+	 
+   it "Deve retornar apenas Disciplinas Fixas" do
+     Disciplina.fixa(:all).length.should == 1
+     Disciplina.find(:all).length.should == 4
+   end
+   
+   it "Deve retornar apenas Disciplinas Normais" do
+      Disciplina.normal(:all).length.should == 2
+      Disciplina.find(:all).length.should == 4
+    end
 end
