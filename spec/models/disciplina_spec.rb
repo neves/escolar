69b240cadc5f23e72e0971bedae1264e94dbbeb7
Marcos Neves <johndoe@example.com>
2008-04-19 07:19:39 -0300

    dúvidas sobre RSpec e BDD em geral

--- spec/models/disciplina_spec.rb
+++ spec/models/disciplina_spec.rb
@@ -1,5 +1,69 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
+=begin
+Este teste está muito grande, como faço para dividí-lo em arquivos separados?
+Como estes arquivos devem ser organizados?
+
+Existe um cenário abaixo que possui a seguinte característica:
+	Vamos pegar o exemplo da pilha:
+		describe Pilha
+			it "quando esta vazia"
+				deve ter zero elementos
+				... outras duzias de testes
+			it "quando recebe 1 elemento"
+				aqui quero chamar o cenário anterior para reaproveitar código, mas negando, ou seja"
+				"quando recebe 1 elemento, não pode se comportar como uma pilha vazia"
+			it "quando remover todos elementos"
+				aqui quero chamar o scenario vazia, pois não quero repetir os testes
+				Esta situação acontece no código abaixo.
+
+Eu percebi que existem factorys de scenarios que gostaria de reaproveitar em outros testes,
+por exemplo: factory.criar_1_professor_e_2_alunos
+então sempre que quiser fazer um teste em um ambiente que eu tenha 1 professor e 2 alunos,
+eu chamo este factory, mas em cada cenario, eu quero poder chamar este factory e alterar algo 
+para customizar.
+
+Veja que utilizei bastante describe dentro de describe. Seria melhor ter utilizado stories?
+
+Também testei na Disciplina, coisas relacionadas ao Professor e a Disponibilidade,
+como organizar isso em arquivos diferentes?
+
+Eu reparei que quando alterava alguns models, tinha que fazer touch neste arquivo para o autotest
+rodar novamente. O que fiz de errado para ele não reconhecer automaticamente a alteração?
+
+Que outro plugin vc utiliza:
+http://blog.jayfields.com/2008/03/ruby-expectations-gem-version-023.html
+http://chneukirchen.org/repos/bacon/
+http://thoughtbot.com/projects/shoulda
+outros...
+
+Em algum lugar abaixo, conseguiria utilizar mock, para não precisar ir ao banco?
+Pode me dar um exemplo, utilizando o sistema abaixo se possível, de como utilizar mock e stub?
+Estas duas técnicas ainda estão confusas pra mim. A teoria eu compreendi.
+
+Como você faz testes para na verdade tirar uma dúvida quanto ao funcionamento do Rails?
+Por exemplo, se eu tenho um Post e um Comentario instanciado
+e estou na dúvida se o Rails implementa Unit of Work, para garantir que o mesmo comentario que tenho
+em um objeto, é o mesmo que está relacionado ao Post, como organizo testes desta categoria?
+
+Todos meus testes estão passando. como rodar um relatório das espectativas,
+como se fosse uma documentação do comportamento do código.
+Acredito que esta prática deve ser muito utilizada, 
+até mesmo para mostrar ao cliente, pois estará na língua dele (regras de negócio)
+
+Os dois únicos controllers desta aplicação, estão sem testes. 
+Vc pode me dar um exemplo de o que devo testar num controller? Uma dificuldade que acho que todos
+tem quando estão começando com testes, é saber o que testar.
+
+em app/helpers/grid_table.rb tem um helper que fiz para me auxiliar à gerar um tipo de tabela muito
+utilizada neste meu projeto. Como faço teste de coisas que geram html 
+e que precisam ter acesso a engine de template, etc? Não acho certo comparar string retornada,
+pois se eu modificar um simples \n ou \t, já tenho que modificar o teste.
+
+Falando nisso, como é o workflow diário de modificar código que exijam mudanças nos testes,
+desde uma simples alteração de o nome de um metodo, até a alteração de uma expectativa do cliente?
+=end
+
 describe Disciplina do
   before(:each) do
     @disciplina = Disciplina.create :fixa => true, :nome => 'Laboratório', :apelido => 'lab'
