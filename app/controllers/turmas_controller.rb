6eed2d913e558260e7d74f28042f6df9fdef3fa8
Marcos Neves <marcos.neves@gmail.com>
2008-05-03 20:46:48 -0300

    filtro de professores

--- app/controllers/turmas_controller.rb
+++ app/controllers/turmas_controller.rb
@@ -1,6 +1,10 @@
 class TurmasController < ApplicationController
   def index
     data = Date.today
-    @turmas = Turma.find(:all, :conditions => {:data => data}, :include => [:professor, :disciplina, :alunos], :order => 'quando')
+    filtro = {:data => data}
+    filtro[:professor_id] = params[:professor_id].to_i if params[:professor_id].to_i > 0
+    @current_professor = filtro[:professor_id]
+    @turmas = Turma.find(:all, :conditions => filtro, :include => [:professor, :disciplina, :alunos], :order => 'quando')
+    @professores = Professor.find(:all, :order => 'nome')
   end
 end
